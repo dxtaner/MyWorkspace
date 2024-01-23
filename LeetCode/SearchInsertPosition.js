14ac2415750e7a5cae13eee9c20797c5c781cec9
Taner Özer <tanerozer16@gmail.com>
2024-01-23 12:03:26 +0300

    Create SearchInsertPosition.js

---
+++ LeetCode/SearchInsertPosition.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+
+        if (nums[mid] === target) {
+            return mid; // Target found at index mid
+        } else if (nums[mid] < target) {
+            left = mid + 1; // Target is in the right half
+        } else {
+            right = mid - 1; // Target is in the left half
+        }
+    }
+
+    // If the loop terminates, the target is not in the array
+    // The left pointer points to the index where the target should be inserted
+    return left;
+};
