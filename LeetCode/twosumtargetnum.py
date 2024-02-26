5387372e7c77c3bea3b463b50a184ffd0e35b10f
taner <tanerozer16@gmail.com>
2024-02-26 04:47:57 +0300

    Create twosumtargetnum.py

---
+++ LeetCode/twosumtargetnum.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+
+        num_dict = {}
+        for i, num in enumerate(nums):
+            if target - num in num_dict:
+                return [num_dict[target - num], i]
+            num_dict[num] = i
+        return []
