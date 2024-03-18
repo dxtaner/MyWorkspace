0e874d28c796af5b08a07c1fbc899b138925e4b4
taner <tanerozer16@gmail.com>
2024-03-18 16:41:34 +0300

    Create zigzagConversion.py

---
+++ LeetCode/zigzagConversion.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def convert(self, s, numRows):
+        """
+        :type s: str
+        :type numRows: int
+        :rtype: str
+        """
+        if numRows == 1 or numRows >= len(s):
+            return s
+
+        result = [''] * numRows
+        index, step = 0, 1
+
+        for char in s:
+            result[index] += char
+
+            if index == 0:
+                step = 1
+            elif index == numRows - 1:
+                step = -1
+
+            index += step
+
+        return ''.join(result)
+
+
+# Example usage:
+solution = Solution()
+print(solution.convert("PAYPALISHIRING", 3))  # Output: "PAHNAPLSIIGYIR"
+print(solution.convert("PAYPALISHIRING", 4))  # Output: "PINALSIGYAHRPI"
+print(solution.convert("A", 1))  # Output: "A"
