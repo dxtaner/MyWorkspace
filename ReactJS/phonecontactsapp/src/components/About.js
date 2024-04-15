afe19123d09d8096242e2320c18a858ce46b7956
taner <tanerozer16@gmail.com>
2024-04-15 14:04:56 +0300

    Create About.js

---
+++ ReactJS/phonecontactsapp/src/components/About.js
@@ -0,0 +1,14 @@
+// About.js
+import React from "react";
+import "./About.css";
+
+function About() {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>Welcome to our website! Learn more about us here.</p>
+    </div>
+  );
+}
+
+export default About;
