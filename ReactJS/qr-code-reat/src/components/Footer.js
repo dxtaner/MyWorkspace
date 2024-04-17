0fe4c462bb9027aaf553e0fbea84272bb0506b13
taner <tanerozer16@gmail.com>
2024-04-17 13:12:36 +0300

    Create Footer.js

---
+++ ReactJS/qr-code-reat/src/components/Footer.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer-container">
+      <p className="footer-text">
+        &copy; Hype 2023. All rights reserved. | Designed with ❤️ by Hype QR
+        Code |
+        <a
+          href="https://github.com/dxtaner"
+          target="_blank"
+          rel="noopener noreferrer">
+          dxtaner
+        </a>
+      </p>
+    </div>
+  );
+}
+
+export default Footer;
