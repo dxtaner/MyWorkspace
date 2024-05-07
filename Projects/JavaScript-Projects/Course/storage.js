338fd8ba0593c361d9d93445ecafedd2933c1674
taner <tanerozer16@gmail.com>
2024-05-07 13:18:20 +0300

    Create storage.js

---
+++ Projects/JavaScript-Projects/Course/storage.js
@@ -0,0 +1,45 @@
+class Storage {
+  static getCourses() {
+    let courses;
+
+    if (localStorage.getItem("courses") === null) {
+      courses = [];
+    } else {
+      courses = JSON.parse(localStorage.getItem("courses"));
+    }
+    return courses;
+  }
+
+  static displayCourses() {
+    const courses = Storage.getCourses();
+
+    courses.forEach((course) => {
+      const ui = new UI();
+      ui.addCourseToList(course);
+    });
+  }
+
+  static addCourse(course) {
+    const courses = Storage.getCourses();
+    courses.push(course);
+    localStorage.setItem("courses", JSON.stringify(courses));
+  }
+
+  static deleteCourse(element) {
+    if (element.classList.contains("delete")) {
+      const id = element.getAttribute("data-id");
+
+      const courses = Storage.getCourses();
+
+      courses.forEach((course, index) => {
+        if (course.courseId == id) {
+          courses.splice(index, 1);
+        }
+      });
+
+      localStorage.setItem("courses", JSON.stringify(courses));
+    }
+  }
+}
+
+export default Storage;
