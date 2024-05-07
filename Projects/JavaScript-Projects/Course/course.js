5813ea5a8abe63b231559ce1a2de8e41e15a2cfe
taner <tanerozer16@gmail.com>
2024-05-07 13:17:58 +0300

    Create course.js

---
+++ Projects/JavaScript-Projects/Course/course.js
@@ -0,0 +1,10 @@
+class Course {
+  constructor(title, instructor, image) {
+    this.courseId = Math.floor(Math.random() * 10000);
+    this.title = title;
+    this.instructor = instructor;
+    this.image = image;
+  }
+}
+
+export default Course;
