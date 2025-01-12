5f2527694c8624e97c36715d2a3d7298892d4fcf
Taner Özer 😌 <tanerozer16@gmail.com>
2025-01-12 03:18:31 +0300

    Update arrayfiltered.js

--- LeetCode/arrayfiltered.js
+++ LeetCode/arrayfiltered.js
@@ -1,15 +1,27 @@
+
 /**
- * @param {number[]} arr
- * @param {Function} fn
- * @return {number[]}
+ * Filters an array based on a given condition function.
+ * 
+ * @param {number[]} arr - The array to filter.
+ * @param {Function} fn - A function that determines whether an element should be included in the result.
+ *                          It takes two arguments:
+ *                          - arr[i]: The current element in the array.
+ *                          - i: The index of the current element.
+ * @return {number[]} - A new array containing elements that satisfy the condition defined by `fn`.
  */
 var filter = function (arr, fn) {
+  // Initialize an empty array to store the filtered elements
   var filteredArray = [];
+
+  // Loop through each element in the array
   for (var i = 0; i < arr.length; i++) {
+    // Check if the current element satisfies the condition defined by `fn`
     if (fn(arr[i], i)) {
+      // If it does, add the element to the filtered array
       filteredArray.push(arr[i]);
     }
   }
 
+  // Return the filtered array
   return filteredArray;
 };
