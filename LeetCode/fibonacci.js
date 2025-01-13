2e227ea0f0bf74c43f329e3e6958fb7bce7202fb
Taner Özer 😌 <tanerozer16@gmail.com>
2025-01-13 20:59:22 +0300

    Update fibonacci.js

--- LeetCode/fibonacci.js
+++ LeetCode/fibonacci.js
@@ -1,13 +1,20 @@
 /**
- * @param {number} n
- * @return {number}
+ * Calculates the n-th Fibonacci number using recursion.
+ * 
+ * @param {number} n - The position of the Fibonacci number to calculate (0-indexed).
+ * @return {number} - The n-th Fibonacci number.
  */
 var fib = function (n) {
+  // Base case: If n is 0, return 0 (first Fibonacci number is 0)
   if (n === 0) {
     return 0;
-  } else if (n === 1) {
+  } 
+  // Base case: If n is 1, return 1 (second Fibonacci number is 1)
+  else if (n === 1) {
     return 1;
-  } else {
+  } 
+  // Recursive case: Return the sum of the two preceding Fibonacci numbers
+  else {
     return fib(n - 1) + fib(n - 2);
   }
 };
