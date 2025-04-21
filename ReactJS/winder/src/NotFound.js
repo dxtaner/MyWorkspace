20498f0198cd9381d3b8de430fa67e06c6884975
Taner Özer 😌 <tanerozer16@gmail.com>
2025-04-21 11:54:06 +0300

    Create NotFound.js

---
+++ ReactJS/winder/src/NotFound.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./NotFound.css"; //
+
+export default class NotFound extends Component {
+  render() {
+    return (
+      <div className="not-found-container">
+        <h2>Sayfa Bulunamadı..</h2>
+      </div>
+    );
+  }
+}
