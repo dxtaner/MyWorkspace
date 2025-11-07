c5babcb130d1d26f929b1a36e90a8a6695e885e2
Taner Özer <tanerozer16@gmail.com>
2025-11-07 17:51:50 +0300

    Create debugging.py

---
+++ Python/PythonPro100days/debugging.py
@@ -0,0 +1,16 @@
+# Bugged
+# number = int(input("Which number do you want to check? "))
+
+# if number % 2 = 0:
+# print("This is an even number.")
+# else:
+# print("This is an odd number.")
+
+
+# Debugged
+number = int(input("Which number do you want to check? "))
+
+if number % 2 == 0:
+    print("This is an even number.")
+else:
+    print("This is an odd number.")
