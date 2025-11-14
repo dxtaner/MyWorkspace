655cab17161351c9cd0715aa2de23bbee98a29f8
Taner Özer <tanerozer16@gmail.com>
2025-11-14 17:46:39 +0300

    Create game_data.py

---
+++ Python/PythonPro100days/Higher-Lower/game_data.py
@@ -0,0 +1,50 @@
+data = [
+    {
+        "name": "Instagram",
+        "follower_count": 646,
+        "description": "Social media platform",
+        "country": "United States"
+    },
+    {
+        "name": "Cristiano Ronaldo",
+        "follower_count": 600,
+        "description": "Footballer",
+        "country": "Portugal"
+    },
+    {
+        "name": "Ariana Grande",
+        "follower_count": 380,
+        "description": "Musician and actress",
+        "country": "United States"
+    },
+    {
+        "name": "Kim Kardashian",
+        "follower_count": 360,
+        "description": "TV personality and businesswoman",
+        "country": "United States"
+    },
+    {
+        "name": "Selena Gomez",
+        "follower_count": 430,
+        "description": "Musician and actress",
+        "country": "United States"
+    },
+    {
+        "name": "Dwayne Johnson",
+        "follower_count": 420,
+        "description": "Actor and professional wrestler",
+        "country": "United States"
+    },
+    {
+        "name": "Taylor Swift",
+        "follower_count": 410,
+        "description": "Musician",
+        "country": "United States"
+    },
+    {
+        "name": "Lionel Messi",
+        "follower_count": 490,
+        "description": "Footballer",
+        "country": "Argentina"
+    },
+]
