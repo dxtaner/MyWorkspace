ab88f32cdb34122997ec4d9ca4edb9ba87761634
Taner Özer <tanerozer16@gmail.com>
2025-11-25 15:51:43 +0300

    Create notification_manager.py

---
+++ Python/PythonPro100days/Flight Deal Tracker/notification_manager.py
@@ -0,0 +1,5 @@
+class NotificationManager:
+    def send(self, message):
+        print("\n===== UCUZ UÇUŞ BULUNDU =====")
+        print(message)
+        print("================================\n")
