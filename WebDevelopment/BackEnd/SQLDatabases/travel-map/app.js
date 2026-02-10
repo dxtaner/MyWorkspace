1f4dc74f234823421de82f034e7adf2d75459f8f
Taner Özer <tanerozer16@gmail.com>
2026-02-10 16:39:18 +0300

    Create app.js

---
+++ WebDevelopment/BackEnd/SQLDatabases/travel-map/app.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes/travelRoutes");
+
+const app = express();
+
+mongoose.connect(process.env.MONGO_URI);
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.use("/", routes);
+
+app.listen(3000, () => console.log("Server started on 3000"));
