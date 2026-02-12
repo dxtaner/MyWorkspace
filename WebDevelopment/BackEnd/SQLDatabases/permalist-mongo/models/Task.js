80d886763edfd789623f54f7e0838aceef5c513c
Taner Özer <tanerozer16@gmail.com>
2026-02-12 13:54:58 +0300

    Create Task.js

---
+++ WebDevelopment/BackEnd/SQLDatabases/permalist-mongo/models/Task.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const taskSchema = new mongoose.Schema({
+  title: String,
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Task", taskSchema);
