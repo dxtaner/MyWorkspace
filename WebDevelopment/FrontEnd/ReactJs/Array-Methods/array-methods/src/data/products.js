90b81c6d062e6f5bb366b677406ec71c9ffa5663
Taner Özer <tanerozer16@gmail.com>
2026-02-27 14:14:21 +0300

    Create products.js

---
+++ WebDevelopment/FrontEnd/ReactJs/Array-Methods/array-methods/src/data/products.js
@@ -0,0 +1,8 @@
+const products = [
+  { id: 1, name: "Laptop", price: 1000 },
+  { id: 2, name: "Phone", price: 600 },
+  { id: 3, name: "Tablet", price: 400 },
+  { id: 4, name: "Monitor", price: 300 },
+];
+
+export default products;
