89a3cf0125ba2cd2ba8899e2c8cc2e94ac3878bf
Taner Özer <tanerozer16@gmail.com>
2026-03-02 17:01:04 +0300

    Create math.js

---
+++ WebDevelopment/FrontEnd/ReactJs/Es6-import-export-and-modules/Es6-import-export-practice/src/math.js
@@ -0,0 +1,13 @@
+export const pi = 3.14159;
+
+export function doublePi() {
+  return pi * 2;
+}
+
+export function triplePi() {
+  return pi * 3;
+}
+
+const square = (x) => x * x;
+
+export default square;
