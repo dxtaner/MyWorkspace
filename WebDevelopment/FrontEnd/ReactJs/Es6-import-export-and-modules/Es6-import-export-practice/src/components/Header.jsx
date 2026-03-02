8dcb43640fcc5dec6802e28c8257a33391e4114c
Taner Özer <tanerozer16@gmail.com>
2026-03-02 17:01:00 +0300

    Create Header.jsx

---
+++ WebDevelopment/FrontEnd/ReactJs/Es6-import-export-and-modules/Es6-import-export-practice/src/components/Header.jsx
@@ -0,0 +1,14 @@
+function Header() {
+  const headingStyle = {
+    color: "purple",
+    textAlign: "center",
+  };
+
+  return (
+    <header>
+      <h1 style={headingStyle}> - Intro to JSX + ES6 Modules</h1>
+    </header>
+  );
+}
+
+export default Header;
