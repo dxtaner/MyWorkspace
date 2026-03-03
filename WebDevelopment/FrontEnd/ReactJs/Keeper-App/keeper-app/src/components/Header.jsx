d1bb4a72c519e9f1b9d28422f99ffff747b0b1a7
Taner Özer <tanerozer16@gmail.com>
2026-03-03 15:32:26 +0300

    Create Header.jsx

---
+++ WebDevelopment/FrontEnd/ReactJs/Keeper-App/keeper-app/src/components/Header.jsx
@@ -0,0 +1,15 @@
+function Header() {
+  return (
+    <header style={headerStyle}>
+      <h1>Keeper</h1>
+    </header>
+  );
+}
+
+const headerStyle = {
+  backgroundColor: "#f5ba13",
+  padding: "16px",
+  color: "white",
+};
+
+export default Header;
