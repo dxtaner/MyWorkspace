24855bb25c32517c138bbe822a354f42c141fb52
Taner Özer <tanerozer16@gmail.com>
2026-03-03 17:14:38 +0300

    Create notes.js

---
+++ WebDevelopment/FrontEnd/ReactJs/Keeper-App/keeper-app-2/vite-project/src/notes.js
@@ -0,0 +1,49 @@
+const notes = [
+  {
+    id: 1,
+    title: "React Props",
+    content: "Props are used to pass data from parent to child components.",
+  },
+  {
+    id: 2,
+    title: "useState Hook",
+    content: "useState allows functional components to manage local state.",
+  },
+  {
+    id: 3,
+    title: "ES6 Arrow Functions",
+    content:
+      "Arrow functions provide a shorter syntax and lexical 'this' binding.",
+  },
+  {
+    id: 4,
+    title: "Node.js API",
+    content:
+      "Building REST APIs with Express makes backend development fast and efficient.",
+  },
+  {
+    id: 5,
+    title: "Machine Learning",
+    content:
+      "Linear Regression and Decision Trees are basic supervised learning algorithms.",
+  },
+  {
+    id: 6,
+    title: "Portfolio Website",
+    content: "A strong portfolio increases visibility in job applications.",
+  },
+  {
+    id: 7,
+    title: "React Native",
+    content:
+      "React Native allows building mobile apps using JavaScript and React.",
+  },
+  {
+    id: 8,
+    title: "GitHub Optimization",
+    content:
+      "Well-structured repositories help recruiters understand your coding style.",
+  },
+];
+
+export default notes;
