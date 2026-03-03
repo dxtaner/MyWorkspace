ce7a37d8c369064e756eac00ffbbf3596a341d48
Taner Özer <tanerozer16@gmail.com>
2026-03-03 15:32:19 +0300

    Create App.jsx

---
+++ WebDevelopment/FrontEnd/ReactJs/Keeper-App/keeper-app/src/App.jsx
@@ -0,0 +1,19 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Note from "./components/Note.jsx";
+
+function App() {
+  return (
+    <div>
+      <Header />
+
+      <Note />
+      <Note />
+      <Note />
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
