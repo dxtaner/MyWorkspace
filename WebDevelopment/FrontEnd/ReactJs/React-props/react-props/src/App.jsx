6b269ad80b450f06190d20d8a76694cc6de31606
Taner Özer <tanerozer16@gmail.com>
2026-03-04 16:11:44 +0300

    Create App.jsx

---
+++ WebDevelopment/FrontEnd/ReactJs/React-props/react-props/src/App.jsx
@@ -0,0 +1,21 @@
+import Card from "./components/Card";
+import contacts from "./contacts";
+
+function App() {
+  return (
+    <div>
+      <h1>Contact List</h1>
+
+      {contacts.map((contact) => (
+        <Card
+          key={contact.id}
+          name={contact.name}
+          phone={contact.phone}
+          email={contact.email}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default App;
