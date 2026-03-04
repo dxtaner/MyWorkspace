0b2fc95f1a53bbbfc6509348bdda726ccc842b91
Taner Özer <tanerozer16@gmail.com>
2026-03-04 16:11:50 +0300

    Create contacts.js

---
+++ WebDevelopment/FrontEnd/ReactJs/React-props/react-props/src/contacts.js
@@ -0,0 +1,22 @@
+const contacts = [
+  {
+    id: 1,
+    name: "Taner Özer",
+    phone: "0555 123 45 67",
+    email: "taner@gmail.com",
+  },
+  {
+    id: 2,
+    name: "Messi Baraca",
+    phone: "000 111 222",
+    email: "Baraca@udemy.com",
+  },
+  {
+    id: 3,
+    name: "React Developer",
+    phone: "999 888 777",
+    email: "react@dev.com",
+  },
+];
+
+export default contacts;
