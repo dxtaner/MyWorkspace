edc10615291f5619d13616e2d4ce444328f6960f
Taner Özer <tanerozer16@gmail.com>
2026-03-04 15:46:07 +0300

    Create emojipedia.js

---
+++ WebDevelopment/FrontEnd/ReactJs/Mapping-components/mapping-components/src/emojipedia.js
@@ -0,0 +1,22 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "😀",
+    name: "Grinning Face",
+    meaning: "A typical smiley face.",
+  },
+  {
+    id: 2,
+    emoji: "😂",
+    name: "Face With Tears of Joy",
+    meaning: "Used when something is extremely funny.",
+  },
+  {
+    id: 3,
+    emoji: "🔥",
+    name: "Fire",
+    meaning: "Something is cool or trending.",
+  },
+];
+
+export default emojipedia;
