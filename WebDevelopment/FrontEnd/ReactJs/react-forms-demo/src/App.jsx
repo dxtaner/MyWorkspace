689328d5956cfa4f396f026b048bf8119fcb45e6
Taner Özer <tanerozer16@gmail.com>
2026-03-05 16:46:43 +0300

    Create App.jsx

---
+++ WebDevelopment/FrontEnd/ReactJs/react-forms-demo/src/App.jsx
@@ -0,0 +1,15 @@
+import ClassForm from "./components/ClassForm";
+import HookForm from "./components/HookForm";
+
+function App() {
+  return (
+    <div>
+      <h1>React Forms - Class vs Hooks</h1>
+      <ClassForm />
+      <hr />
+      <HookForm />
+    </div>
+  );
+}
+
+export default App;
