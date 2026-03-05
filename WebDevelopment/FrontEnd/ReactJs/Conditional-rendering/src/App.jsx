15ed6951f823f1e258bc886a3183b99d6241ad22
Taner Özer <tanerozer16@gmail.com>
2026-03-05 16:32:31 +0300

    Create App.jsx

---
+++ WebDevelopment/FrontEnd/ReactJs/Conditional-rendering/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Conditional Rendering Practice</h1>
+
+      {isLoggedIn ? <h2>Welcome Taner 👋</h2> : <h2>Please Login</h2>}
+
+      <button onClick={() => setIsLoggedIn(!isLoggedIn)}>
+        {isLoggedIn ? "Logout" : "Login"}
+      </button>
+    </div>
+  );
+}
+
+export default App;
