979275c47103c19e011b6f7842615717c5f9350b
Taner Özer <tanerozer16@gmail.com>
2026-03-06 14:05:05 +0300

    Create App.jsx

---
+++ WebDevelopment/FrontEnd/ReactJs/react-state-demo/src/App.jsx
@@ -0,0 +1,18 @@
+import JsxExpressions from "./components/JsxExpressions";
+import ComplexState from "./components/ComplexState";
+import ComplexStatePractice from "./components/ComplexStatePractice";
+
+function App() {
+  return (
+    <div>
+      <h1>React JSX & Complex State</h1>
+      <JsxExpressions />
+      <hr />
+      <ComplexState />
+      <hr />
+      <ComplexStatePractice />
+    </div>
+  );
+}
+
+export default App;
