ff82abe4a804e0373bfac11d13c2f1485c22d42a
Taner Özer <tanerozer16@gmail.com>
2026-03-09 13:11:02 +0300

    Create App.jsx

---
+++ WebDevelopment/FrontEnd/ReactJs/react-component-tree/src/App.jsx
@@ -0,0 +1,18 @@
+import ParentComponent from "./components/ParentComponent";
+import ComponentTreePractice from "./components/ComponentTreePractice";
+
+function App() {
+  return (
+    <div>
+      <h1>Managing a Component Tree</h1>
+
+      <ParentComponent />
+
+      <hr />
+
+      <ComponentTreePractice />
+    </div>
+  );
+}
+
+export default App;
