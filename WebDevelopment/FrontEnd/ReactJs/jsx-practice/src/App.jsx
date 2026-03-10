4fe31e68f0447d88db22e80a362dd62eb0340a43
Taner Özer <tanerozer16@gmail.com>
2026-03-10 16:34:39 +0300

    Create App.jsx

---
+++ WebDevelopment/FrontEnd/ReactJs/jsx-practice/src/App.jsx
@@ -0,0 +1,26 @@
+import Expressions from "./components/Expressions";
+import Styling from "./components/Styling";
+import InlineStyling from "./components/InlineStyling";
+import ReactStylingPractice from "./components/ReactStylingPractice";
+
+function App() {
+  return (
+    <div>
+      <h1>React JSX Practice</h1>
+
+      <Expressions />
+
+      <hr />
+
+      <Styling />
+
+      <hr />
+
+      <InlineStyling />
+      <hr />
+      <ReactStylingPractice />
+    </div>
+  );
+}
+
+export default App;
