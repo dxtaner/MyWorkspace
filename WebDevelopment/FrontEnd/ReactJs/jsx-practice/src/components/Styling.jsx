f2b2f9bbcdfdf3fd374bc0d19ae124e623bd441a
Taner Özer <tanerozer16@gmail.com>
2026-03-10 16:34:49 +0300

    Create Styling.jsx

---
+++ WebDevelopment/FrontEnd/ReactJs/jsx-practice/src/components/Styling.jsx
@@ -0,0 +1,13 @@
+function Styling() {
+  const imgUrl = "https://picsum.photos/200";
+
+  return (
+    <div>
+      <h2>JSX Attributes</h2>
+
+      <img src={imgUrl} alt="Random" width="200" />
+    </div>
+  );
+}
+
+export default Styling;
