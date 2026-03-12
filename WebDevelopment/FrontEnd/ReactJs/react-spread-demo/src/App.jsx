f13d65be6937c9fe8ae035bf1d5277e791aed93b
Taner Özer <tanerozer16@gmail.com>
2026-03-12 16:25:02 +0300

    Create App.jsx

---
+++ WebDevelopment/FrontEnd/ReactJs/react-spread-demo/src/App.jsx
@@ -0,0 +1,18 @@
+import SpreadOperator from "./components/SpreadOperator";
+import SpreadOperatorPractice from "./components/SpreadOperatorPractice";
+
+function App() {
+  return (
+    <div>
+      <h1>React ES6 Spread Operator</h1>
+
+      <SpreadOperator />
+
+      <hr />
+
+      <SpreadOperatorPractice />
+    </div>
+  );
+}
+
+export default App;
